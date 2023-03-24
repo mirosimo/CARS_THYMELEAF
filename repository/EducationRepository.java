7de9c6120c2ad1d32d26201fd3080a7815816093
mirosimo <120422945+mirosimo@users.noreply.github.com>
2023-03-24 12:55:56 +0100

    Delete repository directory

--- repository/EducationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mirosimo.car_showroom.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.mirosimo.car_showroom.model.Education;
-
-public interface EducationRepository  extends JpaRepository<Education, Long>{
-
-}
--- repository/WorkPositionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mirosimo.car_showroom.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.mirosimo.car_showroom.model.WorkPosition;
-
-public interface WorkPositionRepository extends JpaRepository<WorkPosition, Long>{
-
-}
