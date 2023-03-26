9c7fe278b67e4e6f7436b5be6ab5cf581a02cb29
mirosimo <120422945+mirosimo@users.noreply.github.com>
2023-03-26 01:50:10 +0100

    Delete repository directory

--- repository/CarEngineRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mirosimo.car_showroom.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mirosimo.car_showroom.model.CarEngine;
-
-public interface CarEngineRepository extends JpaRepository<CarEngine, Long> {
-	
-}
