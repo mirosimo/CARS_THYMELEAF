8358f08e3d4939b047a859742e5ad913f8789154
mirosimo <120422945+mirosimo@users.noreply.github.com>
2023-03-31 01:22:28 +0200

    Delete repository directory

--- repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mirosimo.car_showroom.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mirosimo.car_showroom.model.Course;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-
-}
