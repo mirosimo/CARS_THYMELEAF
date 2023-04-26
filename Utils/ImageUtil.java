a96bf1e81a8b0ba3f33519bd6468cba21663eeb5
mirosimo <120422945+mirosimo@users.noreply.github.com>
2023-04-26 07:24:18 +0200

    Delete Utils directory

--- Utils/ImageUtil.java
+++
@@ -1,9 +0,0 @@
-package mirosimo.car_showroom2.Utils;
-
-import java.util.Base64;
-
-public class ImageUtil {
-    public String getImgData(byte[] byteData) {
-        return Base64.getMimeEncoder().encodeToString(byteData);
-    }
-}
